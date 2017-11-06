bf1813da3926929d974fcac9daece6d0d91bc820
joycc_liujue <liujue@fiberhome.com>
2017-11-06 15:39:36 +0800

    change the bg1 and some words in share

--- src/App.jsx
+++ src/App.jsx
@@ -347,7 +347,7 @@ class App extends Component {
           { this.state.toggleRightAnswers ? '' : (
             <div style={ cssInJsApp.answers }>
               <div style={ cssInJsApp.show } onClick={ () => {
-                 document.title = `这是来自武汉公安青年的一份测试，我答对了${this.state.rightCount}道题，你敢来挑战吗？`
+                 document.title = `我在武汉公安青年学习十九大精神网络知识竞赛中答对了${this.state.rightCount}道题，一起来学习吧！`
                  this.setState({ shareing: true })
                } }>秀出我的成绩单</div>
               <br/><br/>完善资料赢取神秘礼品<br/><br/>
